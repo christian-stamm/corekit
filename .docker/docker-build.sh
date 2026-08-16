9fd99e43563d6c556c3e10fd3e56616632affaa1
Christian Stamm <c.stamm1997@gmail.com>
2026-08-17 00:53:52 +0200

    wip

---
+++ .docker/docker-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+if [[ -z "$BUILD_PLATFORM" ]]; then
+    echo "Error: toolchain must be specified via BUILD_PLATFORM." >&2
+    echo "Usage: BUILD_PLATFORM=<pico|posix> $0" >&2
+    exit 1
+fi
+
+docker compose -f "${SCRIPT_DIR}/docker-compose.yml" build corekit-base
+docker compose -f "${SCRIPT_DIR}/docker-compose.yml" build toolchain-${BUILD_PLATFORM}
+docker compose -f "${SCRIPT_DIR}/docker-compose.yml" build corekit-dev
