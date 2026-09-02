cd40cabc2afb88f745a02044b5b95473c2c28364
Christian Stamm <c.stamm1997@gmail.com>
2026-09-02 06:00:55 +0000

    gpio

--- demos/hello.cpp
+++ demos/hello.cpp
@@ -1,115 +1,45 @@
-#include <hardware/pio.h>
 #include <pico/stdlib.h>
 
-#include <cstdlib>
-#include <limits>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <span>
-#include <vector>
+#include <format>
 
-#include "corekit/dmadevice.hpp"
 #include "corekit/executor.hpp"
+#include "corekit/gpiodevice.hpp"
 #include "corekit/logger.hpp"
-#include "corekit/piodevice.hpp"
 #include "corekit/task.hpp"
 #include "corekit/time.hpp"
 #include "corekit/uartdevice.hpp"
-#include "loopback.hpp"
 
 using namespace corekit;
 
-class LoopbackProgram : public Pio::Program {
+class GpioTask : public Task {
    public:
-    LoopbackProgram() : Pio::Program(loopback_program) {}
-
-    virtual Pio::NodeConf buildNodeConf(PIO  block,
-                                        uint node,
-                                        uint base) override {
-        Pio::NodeConf ncfg = loopback_program_get_default_config(base);
-        sm_config_set_out_shift(&ncfg, true, true, 32);
-        sm_config_set_in_shift(&ncfg, false, true, 8);
-        return ncfg;
-    }
-};
-
-class PioTask : public Task {
-   public:
-    PioTask(PIO block)
-        : pio_device_(Pio::Node<uint32_t>::requestUnused(block))
-        , program_(std::make_shared<LoopbackProgram>()) {}
-
     virtual VoidResult on_enter(StopToken token) override {
-        pio_device_->deploy(program_);
         return VoidResult();
     }
 
     virtual VoidResult on_run(StopToken token) override {
-        while (!token.stop_requested()) {
-            const uint32_t send = 0x12345678;
-            uint32_t       recv = 0;
-            logger_.info() << std::format("Sending: 0x{:08X}", send);
-            pio_device_->write(send);
-            pio_device_->read(recv);
-            logger_.info() << std::format("Received: 0x{:08X}", recv);
-            Time::sleep(1);
+        for (uint i = 0; i < 8; ++i) {
+            Gpio::configure(i,
+                            false,
+                            false,
+                            GPIO_OUT,
+                            GPIO_OVERRIDE_NORMAL,
+                            GPIO_FUNC_SIO);
         }
 
-        return VoidResult();
-    }
-
-    virtual VoidResult on_leave(StopToken token) override {
-        pio_device_->unload();
-        return VoidResult();
-    }
-
-   private:
-    Logger                   logger_{"PioTask"};
-    Pio::Program::Ptr        program_;
-    Pio::Node<uint32_t>::Ptr pio_device_;
-};
-
-class DmaTask : public Task {
-   public:
-    DmaTask() : dma_device_(Dma::Device::requestUnused()) {
-        Dma::Device::enableIRQ();
-
-        aligned_src_ptr_ = new uint32_t(0);
-        aligned_dst_ptr_ = static_cast<uint32_t*>(
-            aligned_alloc(256 * sizeof(uint32_t), 256 * sizeof(uint32_t)));
-    }
-
-    virtual VoidResult on_enter(StopToken token) override {
-        dma_transfer_ = std::make_shared<Dma::Transfer>(
-            0,
-            DREQ_FORCE,
-            reinterpret_cast<volatile void*>(aligned_src_ptr_),
-            reinterpret_cast<volatile void*>(aligned_dst_ptr_),
-            DMA_ADDRESS_UPDATE_NONE,
-            DMA_ADDRESS_UPDATE_INCREMENT,
-            256,
-            Dma::XferSize::DMA_SIZE_32,
-            Dma::Wrapping::Write,
-            false,
-            false,
-            -1,
-            nullptr);
-
-        return VoidResult();
-    }
-
-    virtual VoidResult on_run(StopToken token) override {
-        if (!dma_device_->process(dma_transfer_)) {
-            logger_.warn() << "DMA device is busy.";
-        }
-
-        uint32_t counter = 0;
+        uint64_t value = 0;
         while (!token.stop_requested()) {
-            *aligned_src_ptr_ = counter++;
+            if (value == 0) {
+                value = 1;
+            }
 
+            Gpio::writeAll(value, 0xFF);
             Time::sleep(1);
-            logger_() << aligned_src_ptr_[0] << " -> " << aligned_dst_ptr_[0];
+
+            value <<= 1;
+            value &= 0xFF;
+
+            logger_.info() << std::format("GpioTask: value {:08b}", value);
         }
 
         return VoidResult();
@@ -120,24 +50,17 @@ class DmaTask : public Task {
     }
 
    private:
-    Logger             logger_{"DmaTask"};
-    Dma::Device::Ptr   dma_device_;
-    Dma::Transfer::Ptr dma_transfer_;
-
-    uint32_t* aligned_src_ptr_ = nullptr;
-    uint32_t* aligned_dst_ptr_ = nullptr;
+    Logger logger_{"GpioTask"};
 };
 
 int main() {
     sleep_ms(10);  // Stabilize
     Logging::reconfigure(std::make_shared<Uart::Device>());
 
-    Executor     executor(2, 2);
-    DmaTask::Ptr dmaTask = std::make_shared<DmaTask>();
-    PioTask::Ptr pioTask = std::make_shared<PioTask>(pio0);
+    Executor      executor(2, 2);
+    GpioTask::Ptr gpioTask = std::make_shared<GpioTask>();
 
-    executor.enqueue(pioTask);
-    executor.enqueue(dmaTask);
+    executor.enqueue(gpioTask);
     executor.launch();
 
     return 0;
